9fe8aaa083a93a4cfb8e25fadec0148e6e422dc5
Christian Rafael <christianparadix@gmail.com>
2025-11-02 22:48:30 -0300

    Basic router

---
+++ src/pages/mnist/PreviewTestImages.js
@@ -0,0 +1,18 @@
+function PreviewTestImages() {
+    return (
+        <>
+            <div className="page-container">
+                <div className="page-header">
+                    Mnist Test Images
+                </div>
+                <div className="page-content">
+                    <div className="image">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PreviewTestImages;
