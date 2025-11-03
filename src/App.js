7994b024fca4f45504a81eae88a938519f1548d3
Christian Rafael <christianparadix@gmail.com>
2025-11-02 23:02:03 -0300

    Finish routing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Home from "./pages/Home"
 import PreviewTestImages from "./pages/mnist/PreviewTestImages";
 
 function Navigation() {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <div className="link" onClick={() => alert("Go Home!")}>
-                        Home
-                    </div>
-                </li>
-                <li>
-                    <div className="link" onClick={() => alert("Preview test images!")}>
-                        Preview test images
-                    </div>
-                </li>
-                <li>
-                    <div className="link" onClick={() => alert("Test Perceptron")}>
-                        Test Perceptron
-                    </div>
-                </li>
-            </ul>
-        </nav>
-    );
+  const pushState = (path) => (e) => {
+    e.preventDefault();
+    window.history.pushState({}, "", path);
+    window.dispatchEvent(new PopStateEvent("popstate"));
+  }
+
+  return (
+    <nav>
+      <ul>
+          <li>
+            <div className="link" onClick={pushState("/")}>
+              Home
+            </div>
+          </li>
+          <li>
+            <div className="link" onClick={pushState("/mnist/test-images")}>
+              Preview test images
+            </div>
+          </li>
+          <li>
+            <div className="link" onClick={pushState("/mnist/test-perceptron")}>
+              Test Perceptron
+            </div>
+          </li>
+      </ul>
+    </nav>
+  );
 }
 
 function Router() {
   const [pathname, setPathname] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handlePopState = () => {
+      setPathname(window.location.pathname);
+    }
+
+    window.addEventListener("popstate", handlePopState);
+    
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
   
   switch(pathname) {
     case "/mnist/test-images":
