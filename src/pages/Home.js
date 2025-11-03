9bc1763d986d6df8e6e68dd48541c8349823a707
Christian Rafael <christianparadix@gmail.com>
2025-11-02 22:41:42 -0300

    Init home and navigation

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <>
+            <div className="page-container">
+                <div className="page-header">
+                    Home Page
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home;
